1e40ec82886f8cc534a83ea96d65621fbe8e9acd
HB <spdhqjtl@gmail.com>
2021-11-25 12:23:31 +0900

    'step1'

---
+++ 02_sub.tf
@@ -0,0 +1,48 @@
+# 가용 영역 a의 public subnet
+resource "aws_subnet" "hb_puba" {
+  vpc_id = aws_vpc.hb_vpc.id
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    "Name" = "hb-puba"
+  }
+}
+
+resource "aws_subnet" "hb_pubc" {
+  vpc_id = aws_vpc.hb_vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    "Name" = "hb-pubc"
+  }
+}
+
+resource "aws_subnet" "hb_pria" {
+  vpc_id = aws_vpc.hb_vpc.id
+  cidr_block = "10.0.2.0/24"
+  tags = {
+    "Name" = "hb-pria"
+  }
+}
+
+resource "aws_subnet" "hb_pric" {
+  vpc_id = aws_vpc.hb_vpc.id
+  cidr_block = "10.0.3.0/24"
+  tags = {
+    "Name" = "hb-pric"
+  }
+}
+
+resource "aws_subnet" "hb_pridba" {
+  vpc_id = aws_vpc.hb_vpc.id
+  cidr_block = "10.0.4.0/24"
+  tags = {
+    "Name" = "hb-pridba"
+  }
+}
+
+resource "aws_subnet" "hb_pridbc" {
+  vpc_id = aws_vpc.hb_vpc.id
+  cidr_block = "10.0.5.0/24"
+  tags = {
+    "Name" = "hb-pridbc"
+  }
+}
