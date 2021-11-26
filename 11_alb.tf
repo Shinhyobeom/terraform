d95061283b2722f5d8adb4b8ec213b9daa7d36d9
hhhbbb <spdhqjtl@gmailcom>
2021-11-26 12:12:15 +0900

    'step1'

---
+++ 11_alb.tf
@@ -0,0 +1,10 @@
+resource "aws_lb" "hb_alb" {
+  name = "hb-alb"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.hb_sg.id]
+  subnets = [aws_subnet.hb_puba.id,aws_subnet.hb_pubc.id]
+  tags = {
+    "Name" = "hb-alb"
+  }
+}
