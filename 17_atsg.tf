3a7e5150dafa438f7b4a55408b6d77435660052a
hhhbbb <spdhqjtl@gmailcom>
2021-11-26 16:52:21 +0900

    'step1'

---
+++ 17_atsg.tf
@@ -0,0 +1,18 @@
+#배치 그룹
+resource "aws_placement_group" "hb_pg" {
+  name = "hb-pg"
+  strategy = "cluster"
+}
+#오토스케일링 그룹
+resource "aws_autoscaling_group" "hb_atsg" {
+  name = "hb-atsg"
+  min_size = 2
+  max_size = 8
+  health_check_grace_period = 60
+  health_check_type = "EC2"
+  desired_capacity = 2
+  force_delete = true
+  #placement_group = aws_placement_group.hb_pg.id
+  launch_configuration = aws_launch_configuration.hb_lacf.name
+  vpc_zone_identifier = [aws_subnet.hb_puba.id,aws_subnet.hb_pubc.id]
+}
