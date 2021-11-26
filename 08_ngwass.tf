97ae5ca3dc65600176a970f53eed5d57052a3c57
hhhbbb <spdhqjtl@gmailcom>
2021-11-26 12:03:44 +0900

    'step2'

--- 08_ngwass.tf
+++ 08_ngwass.tf
@@ -1,19 +1,19 @@
 resource "aws_route_table_association" "hb_ngwass_pria" {
-  subnet_id = aws_subnet.hb_pria.id
+  subnet_id      = aws_subnet.hb_pria.id
   route_table_id = aws_route_table.hb_ngwrt.id
 }
 
 resource "aws_route_table_association" "hb_ngwass_pric" {
-  subnet_id = aws_subnet.hb_pric.id
+  subnet_id      = aws_subnet.hb_pric.id
   route_table_id = aws_route_table.hb_ngwrt.id
 }
 
 resource "aws_route_table_association" "hb_ngwass_pridba" {
-  subnet_id = aws_subnet.hb_pridba.id
+  subnet_id      = aws_subnet.hb_pridba.id
   route_table_id = aws_route_table.hb_ngwrt.id
 }
 
 resource "aws_route_table_association" "hb_ngwass_pridbc" {
-  subnet_id = aws_subnet.hb_pridbc.id
+  subnet_id      = aws_subnet.hb_pridbc.id
   route_table_id = aws_route_table.hb_ngwrt.id
 }
