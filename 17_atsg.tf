f2992459d6bb8f745b525b0c80c563004f165f8e
HB <spdhqjtl@gmail.com>
2021-11-29 09:24:31 +0900

    step2

--- 17_atsg.tf
+++ 17_atsg.tf
@@ -11,7 +11,7 @@ resource "aws_autoscaling_group" "hb_atsg" {
   health_check_grace_period = 60
   health_check_type = "EC2"
   desired_capacity = 2
-  force_delete = true
+  force_delete = false
   #placement_group = aws_placement_group.hb_pg.id
   launch_configuration = aws_launch_configuration.hb_lacf.name
   vpc_zone_identifier = [aws_subnet.hb_puba.id,aws_subnet.hb_pubc.id]
